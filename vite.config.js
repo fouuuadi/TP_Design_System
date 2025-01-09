d0bb5f152037795686eb247659ffb4af71fb212b
fouuuadi <fouadlamnaouar18@gmail.com>
2025-01-09 09:26:16 +0100

    fix: try to fix router for github page

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/TP_Design_System/', 
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: './',
+        },
+      ],
+    }),
+  ],
+  base: '/TP_Design_System/',
 });
