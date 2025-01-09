f67320c7f31e4006f26df27add47cc8c83a9970d
nabil94370 <nabilabbes94370@gmail.com>
2025-01-09 16:01:53 +0100

    connexion login

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -1,30 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "../input/input";
 import InputPassword from "../inputPassword/inputPassword";
 import Button from "../button/button";
-import { Link } from "react-router-dom";
-import "./form.css"
+import { Link, useNavigate } from "react-router-dom";
+import "./form.css";
 
 export default function Formulaire() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "admin@admin.fr" && password === "admin") {
+      navigate("/profil");
+    } else {
+      alert("Invalid email or password!");
+      console.log(email, password)
+    }
+  };
+
   return (
-      <div className="form">
-        <div>
-        <h1>Login</h1>
-        <form action="" method="post">
-          <Input
-            name="Email"
-            type="Email"
-            placeholder="john.doe@gmail.com"
-          ></Input>
-          <InputPassword name="Password" type="password"></InputPassword>
-          <div>
-            <Button label="Login"></Button>
-          </div>
-          <div className="linkForgetPass">
-            <Link  className="forgetLink" to="/forget">Did you forget your password ?</Link>
-          </div>
-        </form>
-        </div>
+    <div className="form">
+    <div>
+    <h1>Login</h1>
+    <form action="" onSubmit={handleSubmit} method="post">
+      <Input
+        name="Email"
+        type="Email"
+        placeholder="john.doe@gmail.com"
+        onChange={(e) => setEmail(e.target.value)}
+      ></Input>
+      <InputPassword name="Password" type="password" onChange={(e) => setPassword(e.target.value)}></InputPassword>
+      <div>
+        <Button label="Login"></Button>
       </div>
+      <div className="linkForgetPass">
+        <Link  className="forgetLink" to="/forget">Did you forget your password ?</Link>
+      </div>
+    </form>
+    </div>
+  </div>
   );
 }
