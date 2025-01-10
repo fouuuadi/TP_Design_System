67556888e4d014a43964882dab47db71d019bb37
Amine Abbes <amine.abbes94370@gmail.com>
2025-01-10 12:57:12 +0100

    cleaning

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Switch from "./components/switch/switch";
 const App = () => {
   const [darkMode, setDarkMode] = useState(false);
 
-  // Effet pour appliquer le mode sombre sur le body et toutes les balises <main>
   useEffect(() => {
     const mainElements = document.querySelectorAll("main");
     const body = document.body;
@@ -14,7 +13,7 @@ const App = () => {
       body.style.backgroundColor = "var(--color-darkmode)";
       body.style.color = "var(--color-light)";
       mainElements.forEach((main) => {
-        main.style.backgroundColor = "var(--color-darkmode)"; // Couleur pour le mode clair
+        main.style.backgroundColor = "var(--color-darkmode)";
         main.style.color = "var(--color-light)"
 
         const labels = main.querySelectorAll("label");
@@ -27,7 +26,7 @@ const App = () => {
       body.style.backgroundColor = "var(--color-light)";
       body.style.color = "var(--color-text-light)";
       mainElements.forEach((main) => {
-        main.style.backgroundColor = "var(--color-light)"; // Couleur pour le mode sombre
+        main.style.backgroundColor = "var(--color-light)";
         main.style.color = "var(--color-darkmode)"
 
         const labels = main.querySelectorAll("label");
--- src/components/pages/forgetpassword/forget.jsx
+++ src/components/pages/forgetpassword/forget.jsx
@@ -1,5 +1,7 @@
-import Formulaire from "./formForget";
 import formImage from "../../../assets/formimageR.webp";
+import InputPassword from "../../inputPassword/inputPassword";
+import Button from "../../button/button"
+import "../../form/form.css"
 import "../login/login.css"
 
 
@@ -9,9 +11,21 @@ export default function Forget() {
       <div className="imageDiv">
         <img src={formImage} alt="Form image" />
       </div>
-      <main className="containerForm">
-        <Formulaire />
-      </main>
+      <div className="containerForm">
+            <div>   
+              <div>
+                <h1>Réinitialisation</h1>
+                <form action="" method="post">
+                  <InputPassword name="Password" type="password" label={"Nouveau mot de passe"}></InputPassword>
+                  <InputPassword name="Password" type="password" label={"Confirmation mot de passe"}></InputPassword>
+                  <div>
+                    <Button label="Submit"></Button>
+                  </div>
+        
+                </form>
+              </div>
+            </div>
+      </div>
     </div>
   );
 }
