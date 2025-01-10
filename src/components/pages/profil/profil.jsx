f751e619c6844b5e6921bd300d057fb925779c0b
fouuuadi <fouadlamnaouar18@gmail.com>
2025-01-10 12:36:37 +0100

    responsive profil page

--- src/components/pages/profil/profil.jsx
+++ src/components/pages/profil/profil.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import SidebarProfil from "../../sidebar/sidebar_profil";
 import "./profil.css";
 import Content from "../../content_profil/content_profil";
+import Header from "../../header/header";
 
 const Profil = () => {
   return (
     <div className="profil-page">
+      <Header/>
       <SidebarProfil />
       <Content />
     </div>
